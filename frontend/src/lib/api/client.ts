2b2e3a85e65a796e1f05387a187bb213d2097077
rai <mj82xx@gmail.com>
2024-10-07 11:25:15 +0900

    format: apply prettier

--- frontend/src/lib/api/client.ts
+++ frontend/src/lib/api/client.ts
@@ -1,4 +1,4 @@
-import createClient from "openapi-fetch";
-import type { paths } from "./schema";
+import createClient from "openapi-fetch"
+import type { paths } from "./schema"
 
-export const client = createClient<paths>({ baseUrl: "https://catfact.ninja/" });
+export const client = createClient<paths>({ baseUrl: "https://catfact.ninja/" })
