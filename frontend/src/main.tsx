5b49cf503075bd927c748706bf55d6301a6dfb7f
rai <96561881+r4ai@users.noreply.github.com>
2024-10-10 16:50:44 +0900

    chore: add tailwindcss

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
 import App from "./App.tsx"
-import "./index.css"
+import "./styles/global.css"
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- #root element is always present
 createRoot(document.getElementById("root")!).render(
