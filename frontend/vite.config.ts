bcfbb2cded65da39b5957a8d28565449917caaac
rai <96561881+r4ai@users.noreply.github.com>
2024-10-10 16:54:11 +0900

    chore: init shadcn/ui

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,13 @@
 import react from "@vitejs/plugin-react-swc"
+import path from "node:path"
 import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(import.meta.dirname, "./src"),
+    },
+  },
 })
