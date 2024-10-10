4c99b9dbea1dc17c61a68f3cb5dcfdb3f3523107
rai <96561881+r4ai@users.noreply.github.com>
2024-10-10 16:37:16 +0900

    chore: add prettier and eslint

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,26 +1,37 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+// @ts-check
+
+import js from "@eslint/js"
+import globals from "globals"
+import reactHooks from "eslint-plugin-react-hooks"
+import reactRefresh from "eslint-plugin-react-refresh"
+import tseslint from "typescript-eslint"
+import gitignore from "eslint-config-flat-gitignore"
+import react from "eslint-plugin-react"
+import perfectionist from "eslint-plugin-perfectionist"
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  gitignore(),
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
       globals: globals.browser,
     },
+  },
+  js.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+  perfectionist.configs["recommended-natural"],
+  // @ts-expect-error eslint-plugin-react types are incorrect
+  react.configs.flat.recommended,
+  react.configs.flat["jsx-runtime"],
+  {
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
     },
