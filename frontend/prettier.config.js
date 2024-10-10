ac3111e0fb232cd3acb67f5754bf11ef210b625d
rai <96561881+r4ai@users.noreply.github.com>
2024-10-10 13:06:47 +0900

    chore: remove nextjs

--- frontend/prettier.config.js
+++
@@ -1,7 +0,0 @@
-// @ts-check
-
-/** @type {import("prettier").Config} */
-export default {
-  semi: false,
-  plugins: ["prettier-plugin-tailwindcss"],
-}
--- frontend/src/lib/api/client.ts
+++
@@ -1,9 +0,0 @@
-import createClient from "openapi-fetch"
-import type { paths } from "openapi/schema"
-
-export const client = createClient<paths>({
-  baseUrl:
-    process.env.MOCK === "true"
-      ? "http://localhost:4010"
-      : "http://localhost:5016",
-})
