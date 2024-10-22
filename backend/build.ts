e53e7f6b03e51c8ae134edeac14c14bfe32d0d34
rai <96561881+r4ai@users.noreply.github.com>
2024-10-22 14:01:34 +0900

    refactor: resolve eslit errors

--- backend/build.ts
+++ backend/build.ts
@@ -1,17 +1,14 @@
 import { build } from "esbuild"
 
-const b = () =>
-  build({
-    banner: {
-      js: "#!/usr/bin/env node",
-    },
-    bundle: true,
-    entryPoints: ["./src/index.ts"],
-    format: "cjs",
-    // For debug
-    minify: false,
-    outfile: "bin",
-    platform: "node",
-  })
-
-Promise.all([b()])
+await build({
+  banner: {
+    js: "#!/usr/bin/env node",
+  },
+  bundle: true,
+  entryPoints: ["./src/index.ts"],
+  format: "cjs",
+  // For debug
+  minify: false,
+  outfile: "bin",
+  platform: "node",
+})
