a6eed78f1145dc07b06060ffdd85fbfbf0974d5a
rai <96561881+r4ai@users.noreply.github.com>
2024-10-22 14:05:48 +0900

    chore(frontend): upgrade build target version to es2022 from es2020

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,9 @@ import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: "es2022"
+  },
   plugins: [
     TanStackRouterVite({
       generatedRouteTree: path.resolve(
@@ -28,5 +31,5 @@ export default defineConfig({
     alias: {
       "@": path.resolve(import.meta.dirname, "./src"),
     },
-  },
+  }
 })
