072cd5ab3ff88022ec1c6de18a46767d058a1061
rai <96561881+r4ai@users.noreply.github.com>
2024-10-31 13:56:04 +0900

    fix: ensure node_modules dirs exists when postinstall

--- backend/src/bin/postinstall.ts
+++ backend/src/bin/postinstall.ts
@@ -4,15 +4,23 @@ import path from "node:path"
 const projectRootDir = path.resolve(import.meta.dirname, "../../..")
 const backendRootDir = path.resolve(projectRootDir, "backend")
 
-const copyPyodideArtifacts = async () => {
-  const rootNodeModulesDir = path.resolve(projectRootDir, "node_modules")
+const ensureBackendNodeModulesDir = async () => {
   const backendNodeModulesDir = path.resolve(backendRootDir, "node_modules")
+  const backendNodeModulesDirStats = await fs
+    .stat(backendNodeModulesDir)
+    .catch(() => null)
+  if (!backendNodeModulesDirStats?.isDirectory()) {
+    await fs.mkdir(backendNodeModulesDir)
+  }
+}
 
+const copyPyodideArtifacts = async () => {
   await fs.symlink(
-    path.resolve(rootNodeModulesDir, "pyodide"),
-    path.resolve(backendNodeModulesDir, "pyodide"),
+    path.resolve(projectRootDir, "node_modules", "pyodide"),
+    path.resolve(backendRootDir, "node_modules", "pyodide"),
     "dir",
   )
 }
 
+await ensureBackendNodeModulesDir()
 await copyPyodideArtifacts()
