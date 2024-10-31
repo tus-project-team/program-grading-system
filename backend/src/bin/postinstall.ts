57c6da350dd130a6bda8535b94e735cf09051228
rai <96561881+r4ai@users.noreply.github.com>
2024-10-31 23:02:28 +0900

    refactor(backend): postinstall script

--- backend/src/bin/postinstall.ts
+++ backend/src/bin/postinstall.ts
@@ -4,22 +4,30 @@ import path from "node:path"
 const projectRootDir = path.resolve(import.meta.dirname, "../../..")
 const backendRootDir = path.resolve(projectRootDir, "backend")
 
+const exists = async (path: string) => {
+  try {
+    await fs.access(path)
+    return true
+  } catch {
+    return false
+  }
+}
+
 const ensureBackendNodeModulesDir = async () => {
   const backendNodeModulesDir = path.resolve(backendRootDir, "node_modules")
-  const backendNodeModulesDirStats = await fs
-    .stat(backendNodeModulesDir)
-    .catch(() => null)
-  if (!backendNodeModulesDirStats?.isDirectory()) {
+  if (!(await exists(backendNodeModulesDir))) {
     await fs.mkdir(backendNodeModulesDir)
   }
 }
 
 const copyPyodideArtifacts = async () => {
-  await fs.symlink(
-    path.resolve(projectRootDir, "node_modules", "pyodide"),
-    path.resolve(backendRootDir, "node_modules", "pyodide"),
-    "dir",
-  )
+  const srcDir = path.resolve(projectRootDir, "node_modules", "pyodide")
+  const targetDir = path.resolve(backendRootDir, "node_modules", "pyodide")
+  const stat = await fs.lstat(targetDir).catch(() => null)
+  if (stat?.isSymbolicLink()) {
+    return
+  }
+  await fs.symlink(srcDir, targetDir, "dir")
 }
 
 await ensureBackendNodeModulesDir()
