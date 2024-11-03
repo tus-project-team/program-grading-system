60d28d854e363aeb67995f840ac54fc427a40f56
rai <96561881+r4ai@users.noreply.github.com>
2024-11-03 13:41:41 +0900

    fix: loading function

--- frontend/src/routes/problems/$problemId/route.tsx
+++ frontend/src/routes/problems/$problemId/route.tsx
@@ -2,15 +2,15 @@ import { $api } from "@/lib/api"
 import { createFileRoute } from "@tanstack/react-router"
 
 export const Route = createFileRoute("/problems/$problemId")({
-  loader: ({ context: { queryClient }, params }) => {
-    queryClient.ensureQueryData(
+  loader: async ({ context: { queryClient }, params }) => {
+    await queryClient.ensureQueryData(
       $api.queryOptions("get", "/api/problems/{problemId}", {
         params: {
           path: { problemId: Number.parseInt(params.problemId) },
         },
       }),
     )
-    queryClient.ensureQueryData(
+    await queryClient.ensureQueryData(
       $api.queryOptions("get", "/api/problems/{problemId}/submissions", {
         params: {
           path: { problemId: Number.parseInt(params.problemId) },
