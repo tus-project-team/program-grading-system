4d429a244e6f7cb94933e55f1e8d067b44a69721
r9i6k0u1 <6322057@ed.tus.ac.jp>
2024-11-20 14:32:50 +0900

    Submission commit

--- frontend/src/routes/problems/$problemId/-components/info-panel/test-cases.tsx
+++ frontend/src/routes/problems/$problemId/-components/info-panel/test-cases.tsx
@@ -1,11 +1,15 @@
 import type { ComponentPropsWithoutRef, FC } from "react"
 
+import { cn } from "@/lib/utils"
+
+
 export type TestCasesProps = ComponentPropsWithoutRef<"div">
 
 // todo: Implement TestCases
 export const TestCases: FC<TestCasesProps> = ({ className, ...props }) => {
+  
   return (
-    <div className={className} {...props}>
+    <div className={cn("grid grid-cols-[auto_lfr_auto]",className)} {...props}>
       WIP: Display test cases
     </div>
   )
