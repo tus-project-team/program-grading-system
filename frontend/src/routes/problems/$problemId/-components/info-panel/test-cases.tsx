673a0034603beae903007df478ee646c73e52475
r9i6k0u1 <6322057@ed.tus.ac.jp>
2024-11-20 16:43:27 +0900

    TestCases commit

--- frontend/src/routes/problems/$problemId/-components/info-panel/test-cases.tsx
+++ frontend/src/routes/problems/$problemId/-components/info-panel/test-cases.tsx
@@ -1,16 +1,26 @@
 import type { ComponentPropsWithoutRef, FC } from "react"
 
+import { Button } from "@/components/ui/button"
 import { cn } from "@/lib/utils"
-
+import { useProblem } from "../../-hooks/use-problem"
 
 export type TestCasesProps = ComponentPropsWithoutRef<"div">
 
 // todo: Implement TestCases
 export const TestCases: FC<TestCasesProps> = ({ className, ...props }) => {
-  
+  const { problem } = useProblem()
   return (
-    <div className={cn("grid grid-cols-[auto_lfr_auto]",className)} {...props}>
-      WIP: Display test cases
+    <div className={cn("grid grid-cols-[auto_lfr_auto]", className)} {...props}>
+      {problem.data?.test_cases.map(({input, output}) => (
+        <Button
+          className="col-span-2 grid grid-cols-subgrid gap-4"
+          size="sm"
+          variant="ghost"
+        >
+          <div className="text-left">入力：{input}</div>
+          <div className="text-left">出力：{output}</div>
+        </Button>
+      ))}
     </div>
   )
 }
