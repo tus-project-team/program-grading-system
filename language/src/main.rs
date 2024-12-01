06a28987ffe14ba3bdb0fa6f91423ba86aaee519
rai <96561881+r4ai@users.noreply.github.com>
2024-12-01 20:05:48 +0900

    feat: implement minimum parser (#266)

--- language/src/main.rs
+++ language/src/main.rs
@@ -1,3 +1,75 @@
+use clap::Parser;
+use language::{
+    parse::parser::Parser as ShuiroParser, tokenize::tokenizer::Tokenizer as ShuiroTokenizer,
+};
+
+#[derive(clap::Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, default_value_t = Mode::Parse)]
+    mode: Mode,
+
+    #[arg(short, long, default_value_t = false)]
+    command: bool,
+
+    source: String,
+
+    #[arg(short, long)]
+    output: Option<String>,
+}
+
+#[derive(Clone)]
+enum Mode {
+    Tokenize,
+    Parse,
+}
+
+impl std::str::FromStr for Mode {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "tokenize" => Ok(Self::Tokenize),
+            "parse" => Ok(Self::Parse),
+            _ => Err("Invalid mode".to_string()),
+        }
+    }
+}
+
+impl std::fmt::Display for Mode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Parse => write!(f, "parse"),
+            Self::Tokenize => write!(f, "tokenize"),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let source = if args.command {
+        args.source
+    } else {
+        std::fs::read_to_string(&args.source).expect("Failed to read source file")
+    };
+    match args.mode {
+        Mode::Tokenize => {
+            let tokens = ShuiroTokenizer::new(source).tokenize();
+            if let Some(output) = args.output {
+                std::fs::write(output, format!("{:#?}", tokens)).expect("Failed to write output");
+            } else {
+                println!("{:#?}", tokens);
+            }
+        }
+        Mode::Parse => {
+            let tokens = ShuiroTokenizer::new(source).tokenize();
+            let ast = ShuiroParser::new(tokens).parse();
+            if let Some(output) = args.output {
+                std::fs::write(output, format!("{:#?}", ast)).expect("Failed to write output");
+            } else {
+                println!("{:#?}", ast);
+            }
+        }
+    }
 }
