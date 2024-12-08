54313d60fd4865110b30aed0eb4474609390e9f0
rai <96561881+r4ai@users.noreply.github.com>
2024-12-08 18:15:52 +0900

    feat: add sidebar footer

--- frontend/src/components/layout/sidebar.tsx
+++ frontend/src/components/layout/sidebar.tsx
@@ -1,13 +1,24 @@
+import logoUrl from "@/assets/logo.jpg"
 import {
   Collapsible,
   CollapsibleContent,
   CollapsibleTrigger,
 } from "@/components/ui/collapsible"
 import * as UI from "@/components/ui/sidebar"
-import { cn } from "@/lib/utils"
 import { Link, useLocation } from "@tanstack/react-router"
 import { cva } from "class-variance-authority"
+import { LogOutIcon, SettingsIcon, UserIcon } from "lucide-react"
 import { FC } from "react"
+
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu"
 
 export type ItemGroup = {
   children: ItemLink[]
@@ -93,6 +104,89 @@ const SidebarMenuItem: FC<MenuItemProps> = ({ item, pathname }) => {
   )
 }
 
+const SidebarHeader = () => {
+  return (
+    <UI.SidebarHeader>
+      <UI.SidebarMenu>
+        <UI.SidebarMenuItem>
+          <UI.SidebarMenuButton asChild size="lg">
+            <Link className="space-x-1" to="/">
+              <Avatar className="flex aspect-square size-8 items-center justify-center rounded-lg">
+                <AvatarImage src={logoUrl} />
+                <AvatarFallback>SH</AvatarFallback>
+              </Avatar>
+              <div className="flex leading-none">
+                <span className="font-semibold">Shuiro</span>
+              </div>
+            </Link>
+          </UI.SidebarMenuButton>
+        </UI.SidebarMenuItem>
+      </UI.SidebarMenu>
+    </UI.SidebarHeader>
+  )
+}
+
+const SidebarFooter = () => {
+  return (
+    <UI.SidebarFooter>
+      <UI.SidebarMenu>
+        <UI.SidebarMenuItem>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <UI.SidebarMenuButton
+                className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+                size="lg"
+              >
+                <Avatar className="size-8 rounded-lg">
+                  <AvatarImage
+                    src="https://example.com" // TODO: replace with actual user avatar
+                  />
+                  <AvatarFallback>
+                    {/* TODO: replace with actual user initial name */}
+                    TY
+                  </AvatarFallback>
+                </Avatar>
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-semibold">
+                    {/* TODO: replace with actual user name */}
+                    山田 太郎
+                  </span>
+                  <span className="truncate text-xs">
+                    {/* TODO: replace with actual user email */}
+                    hello@example.com
+                  </span>
+                </div>
+              </UI.SidebarMenuButton>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56">
+              <DropdownMenuGroup>
+                <DropdownMenuItem>
+                  {/* TODO: link to a profile page */}
+                  <UserIcon />
+                  <span>Profile</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  {/* TODO: link to a settings page */}
+                  <SettingsIcon />
+                  <span>Settings</span>
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                <DropdownMenuItem>
+                  {/* TODO: implement sign out logic or link to a sign out page */}
+                  <LogOutIcon />
+                  <span>Sign out</span>
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </UI.SidebarMenuItem>
+      </UI.SidebarMenu>
+    </UI.SidebarFooter>
+  )
+}
+
 export type SidebarProps = {
   items: Item[]
 }
@@ -106,11 +200,10 @@ export const Sidebar: FC<SidebarProps> = ({ items }) => {
       return pathname
     },
   })
-  console.dir({ items, pathname })
 
   return (
     <UI.Sidebar>
-      <UI.SidebarHeader />
+      <SidebarHeader />
       <UI.SidebarContent>
         <UI.SidebarGroup>
           <UI.SidebarGroupContent>
@@ -124,6 +217,8 @@ export const Sidebar: FC<SidebarProps> = ({ items }) => {
           </UI.SidebarGroupContent>
         </UI.SidebarGroup>
       </UI.SidebarContent>
+      <SidebarFooter />
+      <UI.SidebarRail />
     </UI.Sidebar>
   )
 }
