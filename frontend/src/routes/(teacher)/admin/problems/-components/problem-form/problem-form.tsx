bc5adfe7ec008a7132fb25edb1fab6e81e086040
Sota810 <183704713+Sota810@users.noreply.github.com>
2024-12-12 01:48:38 +0900

    feat(frontend): add edit language

--- frontend/src/routes/(teacher)/admin/problems/-components/problem-form/problem-form.tsx
+++ frontend/src/routes/(teacher)/admin/problems/-components/problem-form/problem-form.tsx
@@ -281,7 +281,7 @@ export const ProblemForm: FC<ProblemFormProps> = ({
               <div className="flex items-end gap-2">
                 <div className="grid gap-2 flex-1">
                   <Label>言語</Label>
-                  <form.Field name="newLanguage">
+                  <form.Field name="supported_languages">
                     {(field) => (
                       <select
                         className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
